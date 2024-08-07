0c55878faeddaef08ae326ec0d5808ff48a1ea93
Jesus Hernandez <jehfebles@gmail.com>
2024-08-07 11:14:43 -0400

    Ajustes de rutas

--- routes/web.php
+++ routes/web.php
@@ -31,30 +31,30 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
-    Route::resource('mantenice/banks', BankController::class);
+    Route::resource('/mantenice/banks', BankController::class);
     Route::get('/mantenice/banks/search/{id}', [BankController::class, 'searchBank'])->name('banks.search');
-    Route::post('mantenice/banks/{id}/update-bank', [BankController::class, 'updateBank'])->name('banks.update-bank');
-    Route::get('mantenice/banks/{id}/delete', [BankController::class, 'deleteBank'])->name('banks.delete-bank');
+    Route::post('/mantenice/banks/{id}/update-bank', [BankController::class, 'updateBank'])->name('banks.update-bank');
+    Route::get('/mantenice/banks/{id}/delete', [BankController::class, 'deleteBank'])->name('banks.delete-bank');
 
     Route::resource('/mantenice/pay-methods', PayMethodController::class);
-    Route::post('mantenice/pay-methods/{id}/update-method', [PayMethodController::class, 'updateMethod'])->name('pay-methods.update-method');
-    Route::get('mantenice/pay-methods/{id}/inactive', [PayMethodController::class, 'inactive'])->name('pay-methods.inactive');
-    Route::get('mantenice/pay-methods/{id}/delete', [PayMethodController::class, 'delete'])->name('pay-methods.delete');
+    Route::post('/mantenice/pay-methods/{id}/update-method', [PayMethodController::class, 'updateMethod'])->name('pay-methods.update-method');
+    Route::get('/mantenice/pay-methods/{id}/inactive', [PayMethodController::class, 'inactive'])->name('pay-methods.inactive');
+    Route::get('/mantenice/pay-methods/{id}/delete', [PayMethodController::class, 'delete'])->name('pay-methods.delete');
 
-    Route::resource('mantenice/countries', CountryController::class);
+    Route::resource('/mantenice/countries', CountryController::class);
 
-    Route::resource('mantenice/rates', RateController::class);
-    Route::get('mantenice/rates/find-rate/{id}', [RateController::class, 'findRate'])->name('rates.find-rate');
-    Route::post('mantenice/rates/update-rate/{id}', [RateController::class, 'updateRate'])->name('rates.update-rate');
+    Route::resource('/mantenice/rates', RateController::class);
+    Route::get('/mantenice/rates/find-rate/{id}', [RateController::class, 'findRate'])->name('rates.find-rate');
+    Route::post('/mantenice/rates/update-rate/{id}', [RateController::class, 'updateRate'])->name('rates.update-rate');
     Route::post('/mantenice/rates/calculate-amount', [RateController::class, 'calculateAmount'])->name('rates.calculate-amount');
-    Route::get('mantenice/rates/{id}/delete', [RateController::class, 'delete'])->name('rates.delete');
+    Route::get('/mantenice/rates/{id}/delete', [RateController::class, 'delete'])->name('rates.delete');
 
 
 
 
     // Clients
-    Route::resource('clients', ClientController::class);
-    Route::resource('clients-accounts', ClientAccountController::class);
+    Route::resource('/clients', ClientController::class);
+    Route::resource('/clients-accounts', ClientAccountController::class);
 
     Route::get('/clients/find-client/{id}', [ClientController::class, 'findClient'])->name('clients.find-client');
     Route::get('/clients/delete/{id}', [ClientController::class, 'delete'])->name('clients.delete');
@@ -69,7 +69,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/clients/search-dni/{dni}', [ClientController::class, 'searchDNI'])->name('clients.search-dni');
 
     // Transfers
-    Route::resource('transfers', TransferController::class);
+    Route::resource('/transfers', TransferController::class);
     Route::get('/transfers/new-transfer/{id}', [TransferController::class, 'newTransfer'])->name('transfers.new-transfer');
     Route::get('/transfers/print-invoice/{id}', [TransferController::class, 'printInvoice'])->name('transfers.print-invoice');
 
