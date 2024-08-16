97ae4f3cd5702625864c04025ec49fa063e21a4a
Jesus Hernandez <jehfebles@gmail.com>
2024-08-16 11:42:10 -0400

    ajustes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (!app()->environment('local')) {
+            $this->app['request']->server->set('HTTPS', true);
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
